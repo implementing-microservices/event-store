b0f990a832d11c01d1da7cfd46ba988a581b78de
Irakli Nadareishvilii <inadarei@users.noreply.github.com>
2019-04-21 20:40:26 -0400

    migrating to quart

--- server.py
+++ server.py
@@ -6,29 +6,34 @@ import json
 import decimal
 from decimal import Decimal
 
-from sanic import Sanic
-from sanic import response
-from sanic.request import RequestParameters
+from quart import Quart, request, jsonify, Response
 
-app = Sanic()
+app = Quart(__name__)
 
 from eventstore import handlers, db
 
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, decimal.Decimal):
+            return str(obj)
+        # Let the base class default method raise the TypeError
+        return json.JSONEncoder.default(self, obj)
+
 # @see: https://implementing-microservices.github.io/event-store/
 @app.route('/events/<event_type>', methods=['POST'])
-async def save_events(request, event_type):
-    events = request.json
+async def save_events(event_type):
+    events = request.get_json(force=True)
 
     resp = await handlers.save_events(event_type, events)
-    return response.json(resp)
+    return jsonify(resp)
 
 @app.route('/events/<event_type>', methods=['GET'])
-async def get_events(request, event_type):
+async def get_events(event_type):
     since = request.args.get('since', '')
     count = request.args.get('count', 10)
 
     resp = await handlers.get_events(event_type, since, count)
-    return response.json(resp)
+    return jsonify(resp)
     # return resp
 
 def init():
